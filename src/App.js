2d79e0fcb7bb8d5c70402b465f5df7d387058732
Martin Mares <78748150+MartinMaresC@users.noreply.github.com>
2024-03-05 16:35:51 -0600

    App.js updated
    scroll and images updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import rocko from './img/rocko.jpeg';
 
 function App() {
+  const [barPixeles, setPixeles] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const pixeles = Math.round(barra2PositioningPixeles.scrollTop);
+      const threshold = 10;
+
+      if (pixeles !== barPixeles && pixeles > threshold && pixeles < 1050 - threshold) {
+        setPixeles(pixeles);
+      }
+    };
+
+    const barra2PositioningPixeles = document.getElementById("barra2");
+
+    if (barra2PositioningPixeles) {
+      barra2PositioningPixeles.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (barra2PositioningPixeles) {
+        barra2PositioningPixeles.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [barPixeles]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='barra' className='barra' style={{ top: `${barPixeles}px` }}></div>
+      <div
+        id='barra2'
+        className='barra2'
+        style={{ overflowY: 'auto', height: '1000px', fontSize: 0, }}
+      >
+        {/* For example, you can add some content to make it scrollable */}
+        <div style={{ height: '2000px', background: 'blue' }}>
+          {[...Array(10)].map((_, index) => (
+            <img key={index} src={rocko} style={{ verticalAlign: 'top' }} alt={`Rocko ${index + 1}`} />
+          ))}
+        </div>
+      </div>
+      <div>{barPixeles}</div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
